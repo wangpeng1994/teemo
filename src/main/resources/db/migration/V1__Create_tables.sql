e838851ba7c924c1149474cf47acf5cabf4d9815
wangpeng1994 <947034046@qq.com>
2019-11-19 17:24:50 +0800

    optimize create_tables sql

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -1,17 +1,20 @@
-create table NEWS (
-    id bigint primary key auto_increment,
-    title text,
-    content text,
-    url varchar(100),
+create table NEWS
+(
+    id         bigint primary key auto_increment,
+    title      text,
+    content    text,
+    url        varchar(100),
     created_at timestamp default now(),
     updated_at timestamp default now()
-);
+) default charset = utf8mb4;
 
-create table LINKS_TO_BE_PROCESSED (
+create table LINKS_TO_BE_PROCESSED
+(
     link varchar(2000)
 );
 
-create table LINKS_ALREADY_PROCESSED (
+create table LINKS_ALREADY_PROCESSED
+(
     link varchar(2000)
 );
 
